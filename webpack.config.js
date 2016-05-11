f8dda679a19f5a183a3095767671e2f8a63203d3
Mark Lozano <mark.d.lozano@gmail.com>
2016-05-10 21:00:32 -0500

    App serving via webpack-dev-server. Reorganized npm scripts slightly

--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,17 @@ module.exports = {
   // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   target: 'web',
 
-  // HMR middleware + entry point for the app
-  entry: [
-    './src/js/index.js'
-  ],
+  // Entry points for the app bundles
+  entry: {
+    app: [
+      './src/js/index.js'
+    ]
+  },
 
   output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: './',
-    filename: 'js/app.js'
+    // Note: Physical files are only output by the production build task `npm run build`.
+    path: path.join(__dirname, '/dist'),
+    filename: 'js/[name].js'
   },
 
   plugins: [
